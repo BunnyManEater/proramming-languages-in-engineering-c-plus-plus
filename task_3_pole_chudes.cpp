db99b4022f421626538bf77f217f5f03655b3855
Valentine <145047474+BunnyManEater@users.noreply.github.com>
2023-09-22 01:28:40 +0300

    Add files via upload

---
+++ task_3_pole_chudes.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+
+
+//функиця проверки слова на корректность 
+
+//функция для зашифровки наверное 
+
+
+int main()
+{
+    std::string word;
+
+    //setlocale(LC_ALL, "Russian"); // russian language works
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    std::cout << "Введие слово!\n" << std::endl;
+    std::cin >> word;
+    std::cout << word;
+
+
+    return 0;
+}   
